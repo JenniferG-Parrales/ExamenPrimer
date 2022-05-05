a7660a75be3e8eddf57902e466a2d563c543117c
Je'nn <85895809+JenniferG-Parrales@users.noreply.github.com>
2022-05-04 21:10:13 -0600

    Add files via upload

---
+++ AppCore/IServices/IWeatherWebService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.IServices
+{
+    public interface IWeatherWebService
+    {
+
+        Task<WeatherForeCast.ForeCastInfo> GetWeatherByGeo(double x, double y, long dt);
+        Task<List<Coordenadas>> GetLatLong(string city);
+    }
+}
